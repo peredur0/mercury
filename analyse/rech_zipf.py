5ca6c5c7c82d7999798f1fb536ff637d3947da14
peredur0 <martial.goehry@gmail.com>
2022-10-02 23:12:57 +0200

    travail sur le rapport, modification de rech_zipf.py pour dissocier chaque graphe de chaque étapes

--- analyse/rech_zipf.py
+++ analyse/rech_zipf.py
@@ -65,6 +65,8 @@ if __name__ == '__main__':
     zbs_const_moyen = np.mean(zbs_const)
     zbs_const_median = np.median(zbs_const)
     print("ok")
+    print(f"Brown moyenne: {zb_const_moyen:.2f}, mediane: {zb_const_median:.2f}")
+    print(f"Brown (- stopwords) moyenne: {zbs_const_moyen:.2f}, mediane: {zbs_const_median:.2f}")
 
     # Recherche du coefficiant
     print("-- Recherche du coefficiant...", end=' ')
@@ -80,18 +82,93 @@ if __name__ == '__main__':
     zbsmeth_cmoy = [stats.cout(zbs_freq, zbsme_th[coef], 'absolue') for coef in ls_coef]
     print("ok")
 
-    print(f"cout min brown moyenne: {min(zbmoth_cmoy)}, median: {min(zbmeth_cmoy)}")
-    print(f"cout min brown (- stopwords) moyenne: {min(zbsmoth_cmoy)}, median: {min(zbsmeth_cmoy)}")
+    print(f"cout min brown moyenne: {min(zbmoth_cmoy):.2f}, median: {min(zbmeth_cmoy):.2f}")
+    print(f"cout min brown (- stopwords) moyenne: {min(zbsmoth_cmoy):.2f}, median: {min(zbsmeth_cmoy):.2f}")
     zbmot_coef_cmin = list(zbmo_th.keys())[zbmoth_cmoy.index(min(zbmoth_cmoy))]
     zbmet_coef_cmin = list(zbme_th.keys())[zbmeth_cmoy.index(min(zbmeth_cmoy))]
 
     zbsmot_coef_cmin = list(zbsmo_th.keys())[zbsmoth_cmoy.index(min(zbsmoth_cmoy))]
     zbsmet_coef_cmin = list(zbsme_th.keys())[zbsmeth_cmoy.index(min(zbsmeth_cmoy))]
 
-    print(f"Coefficient min brown moyenne: {zbmot_coef_cmin}, median: {zbmet_coef_cmin}")
-    print(f"Coefficient min brown (- stopwords) moyenne: {zbsmot_coef_cmin}, median: {zbsmet_coef_cmin}")
+    print(f"Coefficient min brown moyenne: {zbmot_coef_cmin:.2f}, median: {zbmet_coef_cmin:.2f}")
+    print(f"Coefficient min brown (- stopwords) moyenne: {zbsmot_coef_cmin:.2f}, median: {zbsmet_coef_cmin:.2f}")
 
-    # Process graphique
+    # Affichage simple
+    plt.figure("Distribution de Zipf", figsize=(10, 10))
+    plt.subplot(1, 2, 1, title="Distribution de BROWN")
+    plt.scatter(zb_rang, zb_freq, label="réel", marker='+', c="black")
+    plt.ylabel('fréquence')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.subplot(1, 2, 2, title="Distribution de BROWN sans stop")
+    plt.scatter(zbs_rang, zbs_freq, label="réel", marker='+', c='black')
+    plt.xlabel('rang')
+    plt.ylabel('fréquence')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+
+    # distribution des constantes
+    plt.figure("Distribution des constantes théoriques", figsize=(10, 10))
+    plt.subplot(1, 2, 1, title="Distribution constante estimée BROWN")
+    plt.hist(zb_const, label='constante', bins=10, color='black')
+    plt.axvline(zb_const_moyen, label='moyenne', c="blue")
+    plt.axvline(zb_const_median, label='mediane', c="green")
+    plt.xlabel("constante théorique")
+    plt.ylabel("nombre")
+    plt.legend()
+    plt.subplot(1, 2, 2, title="Distribution constante estimée BROWN sans stop")
+    plt.hist(zbs_const, label='distribution', bins=10, color='black')
+    plt.axvline(zbs_const_moyen, label='moyenne', c="blue")
+    plt.axvline(zbs_const_median, label='mediane', c="green")
+    plt.xlabel('constante theorique')
+    plt.ylabel("nombre")
+    plt.legend()
+    plt.show()
+
+
+    # Affichage du coup
+    plt.figure("Cout moyen par coef et constante", figsize=(10, 10))
+    plt.subplot(1, 2, 1, title="Moyenne cout BROWN")
+    plt.plot(ls_coef, zbmoth_cmoy, label="constante moyenne", c='blue')
+    plt.plot(ls_coef, zbmeth_cmoy, label="constante mediane", c='green')
+    plt.xlabel('coefficient')
+    plt.ylabel('cout')
+    plt.legend()
+    plt.subplot(1, 2, 2, title="Moyenne cout BROWN sans stop")
+    plt.plot(ls_coef, zbsmoth_cmoy, label="constante moyenne", c='blue')
+    plt.plot(ls_coef, zbsmeth_cmoy, label="constante mediane", c='green')
+    plt.xlabel('coefficient')
+    plt.ylabel('cout')
+    plt.legend()
+    plt.show()
+
+    # Réel et théorique
+    plt.figure("Distribution réelle et théorique", figsize=(10, 10))
+    plt.subplot(1, 2, 1, title="BROWN avec estimation")
+    plt.scatter(zb_rang, zb_freq, label="réel", marker='+', c="black")
+    plt.plot(zb_rang, zbmo_th[zbmot_coef_cmin], label=f'théo avg, coef {zbmot_coef_cmin:.2f}', c="blue")
+    plt.plot(zb_rang, zbme_th[zbmet_coef_cmin], label=f'théo med, coef {zbmet_coef_cmin:.2f}', c="green")
+    plt.ylabel('fréquence')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.subplot(1, 2, 2, title="BROWN sans stop avec estimation")
+    plt.scatter(zbs_rang, zbs_freq, label="réel", marker='+', c='black')
+    plt.plot(zbs_rang, zbsmo_th[zbsmot_coef_cmin], label=f'théo avg, coef {zbsmot_coef_cmin:.2f}', c="blue")
+    plt.plot(zbs_rang, zbsme_th[zbsmet_coef_cmin], label=f'théo med, coef {zbsmet_coef_cmin:.2f}', c="green")
+    plt.xlabel('rang')
+    plt.ylabel('fréquence')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+
+
+    exit(0)
+    # Process graphique complet
     ligne = 2
     colonne = 4
     pos = 1
@@ -107,9 +184,11 @@ if __name__ == '__main__':
     # Estimation de la constante
     pos += 1
     plt.subplot(ligne, colonne, pos, title="Distribution constante estimée")
-    plt.hist(zb_const, label='distribution', bins=10, color='black')
+    plt.hist(zb_const, label='constante', bins=10, color='black')
     plt.axvline(zb_const_moyen, label='moyenne', c="blue")
     plt.axvline(zb_const_median, label='mediane', c="green")
+    plt.xlabel("constante théorique")
+    plt.ylabel("nombre")
     plt.legend()
     # cout
     pos += 1
@@ -146,6 +225,7 @@ if __name__ == '__main__':
     plt.axvline(zbs_const_moyen, label='moyenne', c="blue")
     plt.axvline(zbs_const_median, label='mediane', c="green")
     plt.xlabel('constante theorique')
+    plt.ylabel("nombre")
     plt.legend()
     # cout
     pos += 1
