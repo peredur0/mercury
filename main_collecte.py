a8801495050e03bd1c915b1914022c46200389d5
peredur0 <martial.goehry@gmail.com>
2022-10-28 23:42:05 +0200

    Schéma base de données Amélioration schéma phase1 déplacement des fonctions sqlite.

--- main_collecte.py
+++ main_collecte.py
@@ -125,165 +125,33 @@ def create_doc_process(categorie, liste):
 
 
 #######################################################################################################################
-#           Statistiques Récolte                                                                                 #
+#           Mise en place des bases de données                                                                        #
 #######################################################################################################################
-def print_stats(categorie, etape, cli):
+def bdd_init_es():
     """
-    affiche les statistiques pour une étape
-    :param categorie: <str> catégorie
-    :param etape: <str> l'étape d'intérêt
-    :param cli: <sqlite.connection> client vers la base sqlite
-    :return: <None>
+    Initialisation de l'index ElasticSearch.
+    Stockage des corps des mails avec le minimum de traitement.
+    :return:
     """
-    print('\t{}, '.format(categorie.upper()), end=' ')
-    cursor = cli.execute("SELECT mails, mots, mots_uniques "
-                         "FROM {} "
-                         "WHERE etape LIKE '{}';".format(categorie, etape))
-    ligne1 = cursor.fetchone()
-    if not ligne1 or len(ligne1) != 3:
-        print("Error :", ligne1)
-        return
-
-    print('mails: {} \tmots: {}\t mots uniques: {}'.format(ligne1[0], ligne1[1], ligne1[2]))
+    pass
 
 
-def stats_recolte(categorie, stats_dict, liste):
+def bdd_init_psql():
     """
-    Fonction de récupération des données stastiques pour mercury
-    :param categorie: <str> ham, spam
-    :param stats_dict: <dict> au format de "stats_temp"
-    :param liste: <list> des chemins à analyser
-    :return: <list> liste des mots uniques pour fusion avec les autres catégories
+    Initialisation de la base de données PostgreSQL
+    Stockage des features et données générées.
+    :return:
     """
-    m_uniq = []
-    for file in tqdm.tqdm(liste,
-                          desc="-- Stats - étape : Récolte {}...".format(categorie),
-                          leave=False,
-                          file=sys.stdout,
-                          ascii=True):
-        try:
-            mots = open(file, 'r').read().split()
-            stats_dict['mots'] += len(mots)
-            for mot in mots:
-                if mot not in m_uniq:
-                    m_uniq.append(mot)
-        except UnicodeDecodeError:
-            continue
-
-    stats_dict['etape'] = "recolte"
-    stats_dict['mots_uniques'] = len(m_uniq)
-    stats_dict['mails'] = len(liste)
-
-    print("-- Stats - étape : Récolte {}... OK".format(categorie))
-    return m_uniq
+    pass
 
 
-def stats_creation_doc(categorie, stats_dict, liste):
+def bdd_init_sqlite():
     """
-    Récupération des infos statistiques après create_document
-    :param categorie: <str> catégorie de mail
-    :param stats_dict: <dict> selon template de "stats_temp"
-    :param liste: <list> liste des documents nettoyés
-    :return: <list> liste des mots uniques pour fusion avec les autres catégories
+    Initialisation de base de donnees SQLite
+    Stockage des informations statistiques lors des étapes du traitement.
+    :return:
     """
-    m_uniq = []
-    for doc in tqdm.tqdm(liste,
-                         desc="-- Stats - étape : Nettoyage {}...".format(categorie),
-                         leave=False,
-                         file=sys.stdout,
-                         ascii=True):
-        stats_dict["mots"] += doc["nb_mots"]
-
-        for mot in doc["message"].split():
-            if mot not in m_uniq:
-                m_uniq.append(mot)
-
-    stats_dict["mots_uniques"] = len(m_uniq)
-    stats_dict["mails"] = len(liste)
-    stats_dict["etape"] = "creation document"
-
-    print("-- Stats - étape : création documents {}... OK".format(categorie))
-    return m_uniq
-
-
-def stats_mise_en_base(categorie, stats_dict, liste):
-    """
-    Récupération des statistiques après la mise en base.
-    ! Car les mails déjà présent en base sont rejetés.
-    :param categorie: <str> categorie de mail
-    :param stats_dict: <dict> selon template de "stats_temp"
-    :param liste: <list> liste des documents extraits de la base ES
-    :return: <list> liste des mots uniques pour fusion avec les autres catégories
-    """
-    m_uniq = []
-    for doc in tqdm.tqdm(liste,
-                         desc="-- Stats - étape : Mise en base {}...".format(categorie),
-                         leave=False,
-                         file=sys.stdout,
-                         ascii=True):
-        stats_dict["mots"] += doc["_source"]["nb_mots"]
-        for mot in doc["_source"]["message"].split():
-            if mot not in m_uniq:
-                m_uniq.append(mot)
-
-    stats_dict["mots_uniques"] = len(m_uniq)
-    stats_dict["mails"] = len(liste)
-    stats_dict["etape"] = "mise en base"
-
-    print("-- Stats - étape : Mise en base {}... OK".format(categorie))
-    return m_uniq
-
-
-def stats_process(etape, data):
-    """
-    Gère le processus de récolte et d'affichage des statistique pour chaque étape
-    :param etape: <str> intitulé de l'étape
-    :param data: <dict> données à traiter {'ham': <list>, 'spam': <list>}
-    :return: <None>
-    """
-    fonctions = {'recolte': stats_recolte,
-                 'creation document': stats_creation_doc,
-                 'mise en base': stats_mise_en_base}
-
-    func = fonctions.get(etape.lower(), None)
-    if not func:
-        print("Erreur : étape {} inconnue, dispo - {}".format(etape, fonctions.keys()), sys.stderr)
-        return
-
-    # Données pour SQLite
-    stats_temp = {
-        'etape': 'template',
-        'mails': 0,
-        'mots': 0,
-        'mots_uniques': 0
-    }
-    stats_spam = stats_temp.copy()
-    stats_ham = stats_temp.copy()
-    stats_globales = stats_temp.copy()
-
-    uniq_spam = func('spam', stats_spam, data.get('spam', []))
-    uniq_ham = func('ham', stats_ham, data.get('ham', []))
-
-    stats_globales['mails'] = stats_spam.get('mails', 0) + stats_ham.get('mails', 0)
-    stats_globales['mots'] = stats_spam.get('mots', 0) + stats_ham.get('mots', 0)
-    stats_globales['mots_uniques'] = len(set(uniq_ham + uniq_spam))
-    stats_globales['etape'] = etape
-
-    # - Mise en base des statistiques
-    sl_client = sqlite_cmd.sl_connect('./databases/sqlite_db/stats_dev.db')
-
-    print("--- Sauvegarde des stats de l'étape: {}...".format(etape), end=' ')
-    sqlite_cmd.sl_insert(sl_client, 'globales', stats_globales)
-    sqlite_cmd.sl_insert(sl_client, 'ham', stats_ham)
-    sqlite_cmd.sl_insert(sl_client, 'spam', stats_spam)
-    print('OK')
-
-    print("Données stats de l'étape: {}:".format(etape))
-    for elem in ['ham', 'spam', 'globales']:
-        print_stats(elem, etape, sl_client)
-
-    sl_client.close()
-    return
+    pass
 
 
 #######################################################################################################################
